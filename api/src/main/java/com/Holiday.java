38327f1ac14a12ac8c7ef2e878e7590ca70d3bf0
Isaac Graciano <isaacgraciano123@gmail.com>
2024-01-21 23:31:54 -0300

    Api Holiday

---
+++ api/src/main/java/com/Holiday.java
@@ -0,0 +1,19 @@
+package com.holidayzer.api;
+
+public class Holiday {
+    private String name;
+    private String date;
+
+    public Holiday(String name, String date) {
+        this.name = name;
+        this.date = date;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+}
